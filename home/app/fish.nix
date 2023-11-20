b93d98ef47d1e4049e598dd848a10b51f25a0b94
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2023-11-20 22:16:23 +0100

    _

--- home/app/fish.nix
+++ home/app/fish.nix
@@ -1,7 +1,22 @@
 {
+  # TODO: Config
+
   programs.fish = {
     enable = true;
 
     interactiveShellInit = "set fish_greeting";
+
+    functions = {
+      ya = {
+        body = ''
+          	set tmp (mktemp -t "yazi-cwd.XXXXX")
+          	yazi --cwd-file="$tmp"
+          	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+          		cd -- "$cwd"
+          	end
+          	rm -f -- "$tmp"
+        '';
+      };
+    };
   };
 }
