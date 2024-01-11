a31d717877aaf861dcca58a9d291116c410c7441
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-01-11 10:40:13 +0100

    _

--- home/app/librewolf.nix
+++ home/app/librewolf.nix
@@ -1,6 +1,17 @@
 {
   home.sessionVariables.BROWSER = "librewolf";
 
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+    };
+  };
   programs.librewolf = {
     enable = true;
 
